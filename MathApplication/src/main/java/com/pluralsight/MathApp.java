3bc0b84be07b52642e62903d1e1276571c8a8c90
KINGZANIFICENT <chefangel.vargas23@gmail.com>
2025-04-14 12:08:02 -0400

    finished first lecture

---
+++ MathApplication/src/main/java/com/pluralsight/MathApp.java
@@ -0,0 +1,29 @@
+package com.pluralsight;
+
+public class MathApp {
+
+    public static void main(String[] args){
+
+        // Question 1:
+        // declare variables here
+        // then code solution
+        // then use System.out.println() to display results
+        // ex: System.out.println("The answer is " + answer);
+        // REPEAT FOR NEXT EXERCISE
+
+        // Question 1:
+        double bobSalary = 70000;
+        double garySalary = 85000;
+
+        double highestSalary;
+
+
+        highestSalary = Math.max(bobSalary, garySalary);
+
+        System.out.println("the answer is " + highestSalary);
+
+
+
+    }
+
+}
