72c837ab4a81e6e4b837d968f9cf0105a7a885eb
KINGZANIFICENT <chefangel.vargas23@gmail.com>
2025-04-14 11:52:20 -0400

    finished first lecture

---
+++ wb1-demo1/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,55 @@
+package com.pluralsight;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello, World!");
+
+        String word1 = "Hello";
+
+        String word2 = "World";
+
+        String greeting;
+
+        greeting = word1 + " " + word2 + "!";
+
+
+        System.out.println(greeting);
+
+        /// ///////////////////////////////////////////
+
+        float taxRate = 0.15f;
+
+        long aLargeNumber = 2222222L;
+
+        int aint;
+
+        long blong;
+
+        aint = 7;
+
+        blong = aint;
+
+        /// ////////////////////////////////////////////
+
+        long aLong;
+        int bInt;
+
+        aLong = 777777777L;
+        bInt = (int) aLong;
+
+
+        
+
+
+
+        System.out.println(bInt);
+
+
+
+
+        }
+
+
+
+
+    }
