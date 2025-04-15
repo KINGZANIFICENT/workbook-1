0210d9bdd56b5bf2df4087243b7b9903f869cfd9
KINGZANIFICENT <chefangel.vargas23@gmail.com>
2025-04-15 16:49:08 -0400

    Completed Mod4 - Exercise 1 - Sandwich Shop

---
+++ SandwichShop/src/main/java/com/pluralsight/SandwichShop.java
@@ -0,0 +1,56 @@
+package com.pluralsight;
+import java.util.Scanner;
+
+public class SandwichShop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double basePrice = 5.45;
+        double largePrice = 8.95;
+        double price;
+
+        System.out.println("Choose A Sandwich size:");
+        System.out.println("(1) Regular ($5.45)");
+        System.out.println("(2) Large ($8.95)");
+        System.out.print("Enter your choice (1 or 2): ");
+
+        int choice = scanner.nextInt();
+
+        if (choice == 1) {
+            price = basePrice;
+        } else if (choice == 2) {
+            price = largePrice;
+        } else {
+            System.out.println("Invalid choice. Restart your order.");
+            return;
+        }
+
+        System.out.print("Enter your age for a possible discount: ");
+        int age = scanner.nextInt();
+
+        if (age <= 17) {
+            price *= 0.90; // 10% off
+        } else if (age >= 65) {
+            price *= 0.80; // 20% off
+        }
+
+        System.out.printf("Amount Owed: $%.2f\n", price);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+//     if(HoursWorked > 40) {
+//float OvertimeHours = HoursWorked - 40;
+//totalPay = (40 * PayRate) + (OvertimeHours * PayRate * 2);
+//        } else {
+//totalPay = HoursWorked * PayRate;
+//        }
